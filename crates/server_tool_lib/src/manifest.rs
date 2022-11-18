71576b80f5397426c54b8f8d41e907b6c6499849
billy4479 <giachi.ellero@gmail.com>
2022-11-18 11:49:57 +0100

    wip

--- crates/server_tool_lib/src/manifest.rs
+++ crates/server_tool_lib/src/manifest.rs
@@ -1,1 +1,69 @@
+use std::{
+    fs::{self, File},
+    path::PathBuf,
+};
 
+use anyhow::{bail, Error, Result};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct VersionManifest {
+    id: String,
+    java_url: String,
+    java_version: u32,
+    sha1: String,
+}
+
+impl VersionManifest {
+    fn get_manifest_path() -> Result<PathBuf> {
+        match dirs::data_local_dir() {
+            Some(dir) => Ok(dir.join("server-tool").join("manifest.json")),
+            None => bail!("no cache directory"),
+        }
+    }
+
+    fn get_version_infos() -> Result<Vec<VersionManifest>> {
+        fs::create_dir_all(dirs::data_local_dir().expect("missing data dir"))?;
+
+        let path = Self::get_manifest_path()?;
+        match fs::metadata(&path) {
+            Ok(meta) => {
+                // TODO: check expired manifest
+                Ok(serde_json::from_reader(File::open(path)?)?)
+            }
+            Err(e) => {
+                let manifest = Self::download_manifest()?;
+                serde_json::to_writer_pretty(File::create(&path)?, &manifest)?;
+                Ok(manifest)
+            }
+        }
+    }
+
+    fn download_manifest() -> Result<Vec<VersionManifest>> {
+        const MANIFEST_URL: &str = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
+        let value: serde_json::Value =
+            serde_json::from_reader(reqwest::blocking::get(MANIFEST_URL)?)?;
+
+        let version_manifests_urls_lambda = || -> Option<Vec<&str>> {
+            let mut result = Vec::<&str>::new();
+
+            for version in value.as_object()?["version"].as_array()? {
+                result.push(version.as_object()?["url"].as_str()?)
+            }
+
+            Some(result)
+        };
+
+        let version_manifests_urls = match version_manifests_urls_lambda() {
+            Some(result) => Ok::<Vec<&str>, Error>(result),
+            None => bail!("error parsing json"),
+        }?;
+
+        // TODO: parallelize here?
+        for manifest_url in version_manifests_urls {
+            // reqwest::blocking::get(manifest_url)
+        }
+
+        todo!()
+    }
+}
--- crates/server_tool_lib/src/server.rs
+++ crates/server_tool_lib/src/server.rs
@@ -2,8 +2,79 @@ use std::path;
 
 use anyhow::Result;
 
+#[derive(Default, Debug)]
+pub enum ServerType {
+    #[default]
+    Vanilla,
+    Fabric,
+}
+
+#[derive(Default, Debug)]
 pub struct Server {
-    name: String,
-    path: path::PathBuf,
-    version: String,
+    pub name: String,
+    pub path: path::PathBuf,
+    pub version: String,
+    pub has_git: bool,
+    pub server_type: ServerType,
+}
+
+impl Server {
+    pub fn find(base_dir: &path::Path) -> Result<Vec<Server>> {
+        let mut result = Vec::<Server>::new();
+
+        for entry_result in base_dir.read_dir()? {
+            let Ok(entry) = entry_result else {
+                continue;
+            };
+
+            if !entry.file_type()?.is_dir() {
+                continue;
+            }
+
+            match Self::detect(&entry.path())? {
+                Some(server) => result.push(server),
+                None => {}
+            }
+        }
+
+        Ok(result)
+    }
+
+    fn detect(path: &path::Path) -> Result<Option<Server>> {
+        const MINECRAFT_JAR: &str = "server.jar";
+        const FABRIC_JAR: &str = "fabric-server-launch.jar";
+        const GIT_FOLDER: &str = ".git";
+
+        let mut result: Server = Default::default();
+        let mut found = false;
+
+        for possible_entry in path.read_dir()? {
+            let Ok(entry) = possible_entry else {
+                continue;
+            };
+
+            if entry.file_type()?.is_dir() {
+                result.has_git = entry.file_name() == GIT_FOLDER;
+            } else if entry.file_name() == MINECRAFT_JAR {
+                result.name = path
+                    .file_name()
+                    .expect("no name")
+                    .to_str()
+                    .expect("string conversion")
+                    .to_string();
+            } else if entry.file_name() == FABRIC_JAR {
+                result.server_type = ServerType::Fabric;
+            }
+        }
+
+        if found {
+            Ok(Some(result))
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn detect_version(jar_path: &path::Path) -> Result<Option<String>> {
+        todo!()
+    }
 }
